177cf56c024455d235218ff26b360127e95be4d4
Cynthia London <cynthe@sbcglobal.net>
2016-11-25 21:33:42 -0600

    env

--- burger/models/index.js
+++ burger/models/index.js
@@ -3,3 +3,4 @@ var fs = require('fs');
 var path = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
+var env       = process.env.NODE_ENV || 'development';
