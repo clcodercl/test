76fd2a2e053db660b72e0c299e0b834bde116a2c
Cynthia London <cynthe@sbcglobal.net>
2016-11-25 21:20:28 -0600

    add

--- burger/models/burger.js
+++ burger/models/burger.js
@@ -2,3 +2,7 @@
 module.exports = function(sequelize, DataTypes) { 
 	var Burger = sequelize.define('Burger', {
     burger_name: DataTypes.STRING,
+    devoured: DataTypes.BOOLEAN
+  }, {
+    classMethods: {
+      associate: function(models) {
