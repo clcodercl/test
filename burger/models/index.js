4c2350b8135c712a8c898d1c131b2b62ef56476b
Cynthia London <cynthe@sbcglobal.net>
2016-11-25 21:50:57 -0600

    readdirSync

--- burger/models/index.js
+++ burger/models/index.js
@@ -11,4 +11,6 @@ if (config.use_env_variable) {
 	 var sequelize = new Sequelize(process.env[config.use_env_variable]);
 	 } else {
 	 	 var sequelize = new Sequelize(config.database, config.username, config.password, config);
-	 	 
+}
+fs
+  .readdirSync(__dirname)	 	 
