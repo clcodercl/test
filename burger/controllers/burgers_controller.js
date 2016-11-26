a45a783425978d5671a01cdd6ee56b5b78516367
Cynthia London <cynthe@sbcglobal.net>
2016-11-25 18:13:08 -0600

    building

--- burger/controllers/burgers_controller.js
+++ burger/controllers/burgers_controller.js
@@ -1,3 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var burger = require('../models/burger.js');
+
+router.get('/', function(req, res) {
+    res.redirect('/burgers');
+});
+
+router.get('/burgers', function(req, res) {
+    burger.selectAll(function(data) {
