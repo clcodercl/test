336dbf87917f72b30d2609249a22a759b9524e4f
Cynthia London <cynthe@sbcglobal.net>
2016-11-25 20:06:57 -0600

    status

--- burger/controllers/burgers_controller.js
+++ burger/controllers/burgers_controller.js
@@ -8,3 +8,19 @@ router.get('/', function(req, res) {
 
 router.get('/burgers', function(req, res) {
     burger.selectAll(function(data) {
+    	  var hbsObject = { burgers: data };
+        res.render('index', hbsObject);
+    });
+});
+
+router.post('/burgers/create', function(req, res) {
+    // editing burger so adds in burger_name
+    Burger.create({burger_name: req.body.burger_name})
+    // passing call result
+    .then(function(newBurger){
+        // logging the result
+        console.log(newBurger);
+        // the redirect
+        res.redirect('/');
+    });
+});
